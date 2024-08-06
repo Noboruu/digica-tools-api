2484e21844bd9a37cf7cde4ddf9bd560e951f575
Noboruu <af.rodrigues1999@gmail.com>
2024-08-06 17:10:59 +0100

    Changed json file generation path Added the last generation to git for external use

--- src/main/java/com/noboruu/digica/starter/DigicaWikiExtractor.java
+++ src/main/java/com/noboruu/digica/starter/DigicaWikiExtractor.java
@@ -20,14 +20,12 @@ public class DigicaWikiExtractor {
     public static void main(String[] args) {
         DigicaWikiConnector connector = new DigicaWikiConnector();
         ObjectWriter ow = new ObjectMapper().findAndRegisterModules().writer().withDefaultPrettyPrinter();
-        String path = "C:\\output\\";
         String fileName = "digicaCards.json";
 
         try {
             DigicaWikiExtraction wikiExtraction = connector.getAllCardsFromDigicaWiki();
             String cardSetsJson = ow.writeValueAsString(wikiExtraction);
-            Files.createDirectories(Paths.get(path));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(path + fileName));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
             writer.write(cardSetsJson);
             writer.close();
         } catch (IOException ex) {
