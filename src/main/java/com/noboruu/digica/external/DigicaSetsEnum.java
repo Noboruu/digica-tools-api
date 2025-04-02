61169240642046e702d883250aebfdef1cd48529
Noboruu <af.rodrigues1999@gmail.com>
2025-04-02 01:29:18 +0100

    Added LM-04 Added more BT21 cards

--- src/main/java/com/noboruu/digica/external/DigicaSetsEnum.java
+++ src/main/java/com/noboruu/digica/external/DigicaSetsEnum.java
@@ -49,6 +49,7 @@ public enum DigicaSetsEnum {
     // LM2 omitted because it only has reprints
     LM3("LM", "/wiki/LM-03:_Limited_Card_Set_2024"),
     LM4("LM", "/wiki/LM-04:_Limited_Card_Pack_Torrid_Weiss"),
+    LM5("LM", "/wiki/LM-05:_Limited_Card_Pack_Final_Elysion"),
     // -- Starters Deck --
     ST1("ST1", "/wiki/ST-1:_Starter_Deck_Gaia_Red"),
     ST2("ST2", "/wiki/ST-2:_Starter_Deck_Cocytus_Blue"),
